b6700492a06ec80809eaaf81578d709075519d3b
ZaidKhan15 <zaid159080@gmail.com>
2021-10-12 00:53:37 +0530

    added screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:catalog_app/Screens/homepage.dart';
+import 'package:catalog_app/Screens/loginpage.dart';
 import 'package:flutter/material.dart';
 import 'Screens/homepage.dart';
 
@@ -14,7 +15,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Homepage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      routes: {
+        "/": (context) => Loginpage(),
+        "/home": (context) =>Homepage(),
+      },
     );
+    
   }
 }
     
