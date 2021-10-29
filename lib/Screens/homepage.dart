a9d48bbc8b3aa32d4ed59a09b6b4099c32f251f5
ZaidKhan15 <zaid159080@gmail.com>
2021-10-29 10:46:09 +0530

    Homescreen products loaded

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -2,11 +2,33 @@ import 'package:catalog_app/modals/catalog.dart';
 import 'package:flutter/material.dart';
 import 'package:catalog_app/Widgets/drawer.dart';
 import 'package:catalog_app/Widgets/item_widget.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   
 
   
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+
+    void initState(){
+      super.initState();
+      loadData();
+    }
+   
+   loadData()async
+   {
+      final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+      final decodeJson = jsonDecode(catalogJson);
+      var productData = decodeJson["products"];
+      
+
+   }
+
   Widget build(BuildContext context) {
     final dummylist =List.generate(50,(index)=> CatalogModel.items[0]);
     return  Scaffold(
