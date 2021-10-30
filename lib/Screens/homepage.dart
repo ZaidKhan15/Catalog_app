60727392a8c2b5c4bb5c765529f23425b889e621
ZaidKhan15 <zaid159080@gmail.com>
2021-10-31 02:54:17 +0530

    Completed

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -1,9 +1,11 @@
+import 'package:catalog_app/Widgets/themes.dart';
 import 'package:catalog_app/modals/catalog.dart';
 import 'package:flutter/material.dart';
 import 'package:catalog_app/Widgets/drawer.dart';
 import 'package:catalog_app/Widgets/item_widget.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
+import 'package:velocity_x/velocity_x.dart';
                             
 class Homepage extends StatefulWidget {
   @override
@@ -29,62 +31,30 @@ class _HomepageState extends State<Homepage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text(
-        "Catalog",
-        style: TextStyle(color: Colors.black),
-      )),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 16,
-                    crossAxisSpacing: 16),
-                itemBuilder: (context, index) {
-                  final item = CatalogModel.items[index];
-                  return Card(
-                    clipBehavior: Clip.antiAlias,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12)),
-                    child: GridTile(
-                      header: Container(
-                        child: Text(
-                          item.name,
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        padding: const EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          color: Colors.deepPurple,
-                        ),
-                      ),
-                      child: Container(
-                          child: Image.network(
-                        item.image,
-                        fit: BoxFit.cover,
-                      )),
-                      footer: Container(
-                        height: 23,
-                        decoration: BoxDecoration(
-                          color: Colors.black,
-                          
-                        ),
-                        child: Text(
-                          item.price.toString(),
-                          style: TextStyle(color: Colors.white),textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                  );
-                },
-                itemCount: CatalogModel.items.length,
-              )
-            : Center(
-                child: CircularProgressIndicator(),
-              ),
-      ),
-      drawer: MyDrawer(),
+    
+      body: SafeArea(
+        child: Container(
+          padding: Vx.m32,
+          child: CatalogHeader(),
+        ),
+      )
+    );
+  }
+}
+
+class CatalogHeader extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              "ShoppZone".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
+              "Trending Products".text.xl2.make()
+            ],
+          ),
+      
     );
   }
 }
