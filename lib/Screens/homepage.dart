951f003754ec6a0fbe4949681bc2936f9a477f9a
ZaidKhan15 <zaid159080@gmail.com>
2021-10-31 01:42:07 +0530

    basic homepage completed

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -4,7 +4,7 @@ import 'package:catalog_app/Widgets/drawer.dart';
 import 'package:catalog_app/Widgets/item_widget.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
-
+                            
 class Homepage extends StatefulWidget {
   @override
   State<Homepage> createState() => _HomepageState();
@@ -37,17 +37,50 @@ class _HomepageState extends State<Homepage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatalogModel.items.length,
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16),
                 itemBuilder: (context, index) {
-                  return ItemWidget(
-                    item: CatalogModel.items[index],
+                  final item = CatalogModel.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)),
+                    child: GridTile(
+                      header: Container(
+                        child: Text(
+                          item.name,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                      child: Container(
+                          child: Image.network(
+                        item.image,
+                        fit: BoxFit.cover,
+                      )),
+                      footer: Container(
+                        height: 23,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          
+                        ),
+                        child: Text(
+                          item.price.toString(),
+                          style: TextStyle(color: Colors.white),textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
                   );
                 },
+                itemCount: CatalogModel.items.length,
               )
-            :
-            
-             Center(
+            : Center(
                 child: CircularProgressIndicator(),
               ),
       ),
